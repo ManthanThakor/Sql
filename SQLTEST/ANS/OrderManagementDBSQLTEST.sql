1158c8f77ad6f637ef79963212422a1ccc62147c
ManthanThakor <thakormanthan849@gmail.com>
2025-01-27 16:59:00 +0530

    change

--- SQLTEST/ANS/OrderManagementDBSQLTEST.sql
+++ SQLTEST/ANS/OrderManagementDBSQLTEST.sql
@@ -220,7 +220,10 @@ WHERE commission BETWEEN 0.12 AND 0.14;
 
 SELECT ord_no, purch_amt, ord_date, customer_id, salesman_id
 FROM Tbl_Orders
-WHERE purch_amt BETWEEN 500 AND 4000 AND purch_amt NOT IN (948.50, 1983.43);
+WHERE purch_amt 
+BETWEEN 500 AND 4000 
+AND 
+purch_amt NOT IN (948.50, 1983.43);
 
 --======================================================
 
